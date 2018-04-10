64ab0b37b859d264e288d03548402dee70ce0f71
Blowing <1072878103@qq.com>
2018-04-10 13:29:13 +0800

    搜索

--- app/src/main/java/com/wujie/mobshare/MainActivity.java
+++ app/src/main/java/com/wujie/mobshare/MainActivity.java
@@ -2,6 +2,9 @@ package com.wujie.mobshare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.share_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showShare();
+            }
+        });
+    }
 
+    private void showShare() {
+        OnekeyShare oks = new OnekeyShare();
+        oks.disableSSOWhenAuthorize();
+        oks.setTitle(getString(R.string.ssdk_oks_share));
+        oks.setTitleUrl("http://www.baidu.com");
+        oks.setText("哈哈，你是猜猜我在做什么");
+        oks.show(this);
     }
 }
---
+++ app/src/main/java/com/wujie/mobshare/MobApplication.java
@@ -0,0 +1,10 @@
+package com.wujie.mobshare;
+
+import android.app.Application;
+
+/**
+ * Created by wujie on 2018/4/9/009.
+ */
+
+public class MobApplication extends Application {
+}
